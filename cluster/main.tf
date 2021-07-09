cbff2003f80ba7706c8d1370d99a8bd8476e0edd
BeratUlualan <68704574+BeratUlualan@users.noreply.github.com>
2021-07-09 14:20:40 +0300

    Create main.tf

---
+++ cluster/main.tf
@@ -0,0 +1,30 @@
+resource "aws_instance" "leader_node" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_pair_name
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = var.security_group_ids
+  tags                   = merge({ Name = "${var.cluster_name} 1" }, var.tags)
+
+  # This user_data will be used to instruct the leader_node to form a cluster
+  # with the other nodes.
+  user_data = jsonencode({
+    node_ips     = aws_instance.node.*.private_ip
+    cluster_name = var.cluster_name
+  })
+}
+
+resource "aws_instance" "node" {
+  count = var.node_count - 1
+
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_pair_name
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = var.security_group_ids
+  tags                   = merge({ Name = "${var.cluster_name} ${count.index + 2}" }, var.tags)
+}
+
+locals {
+  all_nodes = concat([aws_instance.leader_node], aws_instance.node.*)
+}
