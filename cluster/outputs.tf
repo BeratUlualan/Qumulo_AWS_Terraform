8f83e04ed4eaa8fe5eb9a469a1fc26a31593c61b
BeratUlualan <68704574+BeratUlualan@users.noreply.github.com>
2021-07-09 14:21:15 +0300

    Create outputs.tf

---
+++ cluster/outputs.tf
@@ -0,0 +1,19 @@
+output "instance_ids" {
+  value       = local.all_nodes.*.id
+  description = "EC2 Instance IDs for all nodes in the cluster"
+}
+
+output "private_ips" {
+  value       = local.all_nodes.*.private_ip
+  description = "EC2 instance private IPs"
+}
+
+output "public_ips" {
+  value       = local.all_nodes.*.public_ip
+  description = "EC2 instance public IPs (if set)"
+}
+
+output "temp_admin_password" {
+  value       = aws_instance.leader_node.id
+  description = "Temporary admin password for the Web UI and API"
+}
