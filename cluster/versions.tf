d5ab1c83d423c19915b566d7af844098a8774f3f
BeratUlualan <68704574+BeratUlualan@users.noreply.github.com>
2021-07-13 22:08:05 +0300

    Update versions.tf

--- cluster/versions.tf
+++ cluster/versions.tf
@@ -2,6 +2,12 @@ terraform {
   required_version = ">= 0.12"
 
   required_providers {
-    aws = "~> 2.53"
+    aws = {
+      version = ">= 3.40"
+    }
   }
+}
+
+provider "aws" {
+  region = var.aws_region
 }
