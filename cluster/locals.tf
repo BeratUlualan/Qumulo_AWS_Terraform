e70ca0be6710db76169ec589fb5a4ec3a085929a
BeratUlualan <68704574+BeratUlualan@users.noreply.github.com>
2021-07-14 12:57:20 +0300

    Update locals.tf

--- cluster/locals.tf
+++ cluster/locals.tf
@@ -1,14 +1,29 @@
 locals {
-  digits     = split(".", "${var.floating_ips}")
-  last_digit = split("-", local.digits[3])
-  range_list = range(local.last_digit[0], local.last_digit[1] + 1)
+  digits1     = split(".", "${var.floating_ips}")
+  last_digit1 = split("-", local.digits1[3])
+  range_list1 = range(local.last_digit1[0], local.last_digit1[1] + 1)
 
   floating_ip = [
-    for i in local.range_list : [
-      join(".", [local.digits[0], local.digits[1], local.digits[2], i])
+    for i in local.range_list1 : [
+      join(".", [local.digits1[0], local.digits1[1], local.digits1[2], i])
     ]
   ]
 
   floating_ip_list = flatten(local.floating_ip)
+
+}
+
+locals {
+  digits2     = split(".", "${var.persistent_ips}")
+  last_digit2 = split("-", local.digits2[3])
+  range_list2 = range(local.last_digit2[0], local.last_digit2[1] + 1)
+
+  persistent_ip = [
+    for i in local.range_list2 : [
+      join(".", [local.digits2[0], local.digits2[1], local.digits2[2], i])
+    ]
+  ]
+
+  persistent_ip_list = flatten(local.persistent_ip)
 
 }
