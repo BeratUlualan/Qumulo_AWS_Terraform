1b5a2fe11b721eb74b71b338450273248bdff97b
BeratUlualan <68704574+BeratUlualan@users.noreply.github.com>
2021-07-14 12:55:45 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,17 @@
 module "qumulo_cluster" {
-  source        = "./cluster"
-  aws_region    = "us-east-1"
-  aws_az        = "us-east-1a"
-  node_type     = "af"
-  cluster_name  = "berat-qumulo"
-  instance_type = "m5.xlarge"
-  node_count    = 4
-  key_pair_name = "bulualan2"
-  #vpc = ""
-  subnet_id          = module.qumulo_cluster.subnet_main
-  security_group_ids = module.qumulo_cluster.security_group_main
+  source             = "./cluster"
+  aws_region         = "us-east-1"
+  aws_az             = "us-east-1a"
+  node_type          = "af"
+  cluster_name       = "berat-qumulo"
+  instance_type      = "m5.xlarge"
+  node_count         = 4
+  key_pair_name      = "bulualan2"
+  vpc_id             = ""
+  subnet_id          = ""
+  security_group_ids = []
   cidr_block         = "10.0.0.0/16"
-  floating_ips = "10.0.0.9-20"
+  persistent_ips     = "10.0.0.5-8"
+  floating_ips       = "10.0.0.9-20"
+  route53_zone = "test-berat.local"
 }
